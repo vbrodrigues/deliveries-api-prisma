3b30b7956f076f065d14716832b0358c81424dbe
Vitor Rodrigues <vitorborbarodrigues@gmail.com>
2022-01-15 17:37:06 -0300

    feat: not bringing client password on find client deliveries endpoint

--- src/modules/clients/useCases/findClientDeliveries/FindClientDeliveriesUseCase.ts
+++ src/modules/clients/useCases/findClientDeliveries/FindClientDeliveriesUseCase.ts
@@ -3,7 +3,7 @@ import { prisma } from '../../../../database/prismaClient';
 
 export class FindClientDeliveriesUseCase {
     async execute(client_id: string) {
-        const deliveries = await prisma.clients.findMany({ where: { id: client_id }, include: { deliveries: true } });
+        const deliveries = await prisma.clients.findMany({ where: { id: client_id }, select: { id: true, username: true, deliveries: true } });
         return deliveries
     }
 }
